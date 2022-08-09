27bf1a82fad554f37696dc434c157a81abed3998
Danilo Mendes <danilo.mirandha@gmail.com>
2022-08-08 21:56:47 -0300

    Iniciando projeto 7

---
+++ Jogo_de_aventura.py
@@ -0,0 +1,8 @@
+# Projeto 7 - Jogo de aventura
+# Criar um jogo de decisões onde deverá ter varios finais
+# diferentes de acordo com as respostas dadas
+
+
+class JogoDeAventura:
+    def __init__(self):
+        self.
